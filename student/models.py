95faf2174780b0007af67473842396c908cdd401
prasantapy <prasanta.sethi.py@gmail.com>
2025-07-07 18:52:13 +0530

    Update models.py

--- student/models.py
+++ student/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Student_model(models.Model):
-    name=models.CharField(max_length=100)
+    name=models.CharField(max_length=200)
     roll=models.IntegerField()
     city=models.CharField(max_length=100)
